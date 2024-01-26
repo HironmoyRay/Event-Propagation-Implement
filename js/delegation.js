3334635ea3cd589d23cab7da09a25ad4026e09a2
hironmoy-ray <hpray35@gmail.com>
2024-01-26 12:05:17 +0600

    Using DOM Element matches()

--- js/delegation.js
+++ js/delegation.js
@@ -1,5 +1,7 @@
 const List = document.getElementById("list");
 List.addEventListener("click",(e) => {
     // console.log(e.target.innerText);
-    e.target.style.backgroundColor = "red";
+    if(e.target.matches("li")){
+        e.target.style.backgroundColor = "red";
+    }
 })
